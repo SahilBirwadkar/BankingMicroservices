baafd791e29ea92da3070f346697fa8ca759b5ec
sahilbirwadkar_0f0dbef6006b <sahil.birwadkar@gmail.com>
2024-12-06 00:30:14 +0530

    OAuth2 Code - Start

---
+++ Section_11/cards/src/main/java/com/sahil/programming/cards/audit/AuditAwareImpl.java
@@ -0,0 +1,15 @@
+package com.sahil.programming.cards.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("CARDS_MS");
+    }
+}
