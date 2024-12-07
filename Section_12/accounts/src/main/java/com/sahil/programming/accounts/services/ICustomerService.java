ad6a8515eeeb4e573d1ea9a25ee5b4f2703f7871
SahilBirwadkar <54496426+SahilBirwadkar@users.noreply.github.com>
2024-12-07 13:51:36 +0530

    Delete Section_12 directory

--- Section_12/accounts/src/main/java/com/sahil/programming/accounts/services/ICustomerService.java
+++
@@ -1,7 +0,0 @@
-package com.sahil.programming.accounts.services;
-
-import com.sahil.programming.accounts.dtos.CustomerDetailsDto;
-
-public interface ICustomerService {
-    CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationId);
-}
